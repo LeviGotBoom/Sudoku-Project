601abae8dd8741f9c57f853390a143af28457a37
anballa <ananya.balla@gmail.com>
2024-11-18 17:24:10 -0500

    Add files via upload

---
+++ sudoku.py
@@ -0,0 +1,13 @@
+import pygame
+from sudoku_generator import *
+
+class SudokuGenerator:
+    def __init__(self, row_length, removed_cells):
+        self.row_length = row_length
+        self.removed_cells = removed_cells
+
+class Cell:
+
+Class Board:
+
+def main():
