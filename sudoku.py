91d593feec78cbec1645c64fd0678ab109056179
Adora <adoraloin@gmail.com>
2024-11-27 12:22:50 -0500

    added welcome text

--- sudoku.py
+++ sudoku.py
@@ -33,16 +33,25 @@ class Board:
     pass
 #add functions
 
-
-PINK = (255, 182, 193)
+pygame.init()
+PINK = (255, 237, 241)
+FONT_COLOR = (0, 0, 0)
+WIDTH = 600
+HEIGHT = 650
+START_FONT = pygame.font.Font(None, 60)
 
 def main():
-    pygame.init()
     screen = pygame.display.set_mode((600, 650))
     pygame.display.set_caption("Sudoku")
 
 
     screen.fill(PINK)
+
+    start_text = "Welcome to Sudoku"
+    end_surf = START_FONT.render(start_text, 0, FONT_COLOR)
+    end_rect = end_surf.get_rect(center=(WIDTH // 2, HEIGHT // 2 - 200))
+    screen.blit(end_surf, end_rect)
+
     pygame.display.update()
 
 
